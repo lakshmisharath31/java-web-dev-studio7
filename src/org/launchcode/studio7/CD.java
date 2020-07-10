9c55069e95f39792f17d7aa2d4836e767e55dac1
lakshmisharath31 <62305053+lakshmisharath31@users.noreply.github.com>
2020-07-09 21:40:25 -0400

    Update CD.java

--- src/org/launchcode/studio7/CD.java
+++ src/org/launchcode/studio7/CD.java
@@ -12,7 +12,3 @@ public class CD extends BaseDisc implements IOpticalDisc {
     }
 
 }
-// TODO: Implement your custom interface.
-
-// TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
-//  need to be declared separately.
