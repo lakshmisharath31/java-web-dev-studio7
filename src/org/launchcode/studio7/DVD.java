43a8a8dff07235c29384ef3c4820613d450ae23c
lakshmisharath31 <62305053+lakshmisharath31@users.noreply.github.com>
2020-07-09 21:41:20 -0400

    Update DVD.java

--- src/org/launchcode/studio7/DVD.java
+++ src/org/launchcode/studio7/DVD.java
@@ -17,9 +17,6 @@ public class DVD extends BaseDisc implements IOpticalDisc {
 
     }
 
-    // TODO: Implement your custom interface.
-
-    // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
-    //  need to be declared separately.
+    
 
 }
