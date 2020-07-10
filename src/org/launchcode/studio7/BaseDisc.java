d01ed36abb5ae7c5920f427453b43df9a510db6f
lakshmisharath31 <62305053+lakshmisharath31@users.noreply.github.com>
2020-07-09 21:44:54 -0400

    Add files via upload

---
+++ src/org/launchcode/studio7/BaseDisc.java
@@ -0,0 +1,14 @@
+package org.launchcode.studio7;
+
+abstract class BaseDisc {
+   private String rpm;
+
+   public BaseDisc(String rpm) {
+      this.rpm=rpm;
+   }
+
+
+   public BaseDisc() {
+
+   }
+}
